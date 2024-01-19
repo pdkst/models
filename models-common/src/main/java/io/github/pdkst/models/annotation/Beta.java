eebd7c547cf9b3f4b789d151919b98a2e0ae1530
pdkst <pdkstudio@163.com>
2024-01-19 23:51:04 +0800

    add Beta

---
+++ models-common/src/main/java/io/github/pdkst/models/annotation/Beta.java
@@ -0,0 +1,21 @@
+package io.github.pdkst.models.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 标记为beta版本，仅标记
+ *
+ * @author pdkst.zhang
+ * @since 2024/01/19
+ */
+@Inherited
+@Documented
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.TYPE})
+public @interface Beta {
+}
