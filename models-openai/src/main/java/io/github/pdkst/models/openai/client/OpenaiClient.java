3ff6ddbc4743ad3027b0b2d40a0968df0505a173
pdkst <pdkstudio@163.com>
2024-04-11 22:12:20 +0800

    rename chat

--- models-openai/src/main/java/io/github/pdkst/models/openai/client/OpenaiClient.java
+++ models-openai/src/main/java/io/github/pdkst/models/openai/client/OpenaiClient.java
@@ -37,7 +37,7 @@ public class OpenaiClient {
         return new OpenaiAudio(httpExchanger);
     }
 
-    public OpenaiChatCompletion chatCompletion() {
+    public OpenaiChatCompletion chat() {
         return new OpenaiChatCompletion(httpExchanger);
     }
 
